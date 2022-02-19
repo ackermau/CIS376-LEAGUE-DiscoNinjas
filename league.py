f79cb1a7505d2e26d8ba57097f05682181472f31
Evan Boardway <evan@Evans-MacBook-Pro.local>
2022-02-19 12:59:09 -0500

    save state

--- league.py
+++ league.py
@@ -1,13 +1,13 @@
 import pygame
 
 
-# Just placed these functions in the class for scaffolding. 
+# Just placed these functions in the class for scaffolding.
 class Engine:
     running = False
     width = 1024
     height = 768
     visible_statistics = False
-    gameDelta = 0
+    delta_time = 0
 
     def __init__(self, title, scene):
         self.title = title
@@ -15,14 +15,29 @@ class Engine:
 
     def toggle_statistics(self):
         self.visible_statistics = True
-    
+
     def show_statistics():
         pass
-    
+
     def init_pygame():
         pass
 
     def run(self):
+
+        background_color = (234, 212, 252)
+
+
+        # Define the dimensions of
+        # screen object(width,height)
+        screen = pygame.display.set_mode((self.width, self.height))
+
+        # Set the caption of the screen
+        pygame.display.set_caption(self.title)
+
+        # Fill the background colour to the screen
+        screen.fill(background_color)
+
+
         self.running = True
 
         targetFrameTime = 1000/self.scene.fps
@@ -34,20 +49,23 @@ class Engine:
             delta = current - last
 
             # Call updataeables
+            for obj in self.scene.updateables:
+                obj.update()
 
             # Call drawables
+            # for obj in self.scene.drawables:
+            #     obj.update()
 
-
+            pygame.display.flip()
 
             # Busy wait until our delta time is equal to our target frame time in ms.
             while delta < targetFrameTime:
                 current = pygame.time.get_ticks()
                 delta = current - last
-                self.gameDelta = delta / 1000
-            
-            print(self.gameDelta)
+                self.delta_time = delta / 1000
 
-    
+
+
     def stop(self):
         self.running = False
 
@@ -57,8 +75,8 @@ class Engine:
 
 
 class Scene:
-    # updateables = []
-    # drawables: pygame.sprite.LayeredDirty = []
+    updateables = []
+    drawables: pygame.sprite.LayeredDirty = []
     fps = 30
     
     def __init__(self, name):

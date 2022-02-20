f689d532d71373b0062bcda5358406430218225c
Evan Boardway <evan@Evans-Mac-Pro.local>
2022-02-20 17:04:49 -0500

    moved ball class

--- main.py
+++ main.py
@@ -1,45 +1,11 @@
 import league
-import pygame as pg
-
-class Ball(league.DUGameObject):
-    def __init__(self, engine):
-        super().__init__()
-        self.engine = engine
-        self._layer = 1
-        self.dirty = 2
-        self.image = pg.Surface((32,32))
-        self.image.fill((255,255,255,0))
-        pg.draw.circle(self.image, (0,101,164), (16, 16), 16)
-        self.rect = self.image.get_rect()
-        self.x = 0
-        self.y = 0
-        self.direction_x = 1
-        self.direction_y = 1
-
-    def update(self):
-        self.x = self.x + self.engine.delta_time * 100 * self.direction_x
-        self.y = self.y + self.engine.delta_time * 100 * self.direction_y
-        self.rect.x = self.x
-        self.rect.y = self.y
-        if self.rect.left < 0:
-            self.rect.left = 0
-            self.direction_x = self.direction_x * -1
-        if self.rect.right > 1023:
-            self.rect.right = 1023
-            self.direction_x = self.direction_x * -1
-        if self.rect.top < 0:
-            self.rect.top = 0
-            self.direction_y = self.direction_y * -1
-        if self.rect.bottom > 768:
-            self.rect.bottom = 768
-            self.direction_y = self.direction_y * -1 
-
+import ball
 
 level_one = league.Scene("Level One")
 level_one.set_fps(60)
 
-engine = league.Engine("EMGGG", level_one)
-ball = Ball(engine)
+engine = league.Engine("Disco Ninjas blue ball          z", level_one)
+ball = ball.Ball(engine)
 level_one.drawables.append(ball)
 level_one.updateables.append(ball)
 

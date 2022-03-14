d9ebc706c3a991d5a8da302de7297b7ad6a90f5f
Collin Turkelson <turkelco@mail.gvsu.edu>
2022-03-13 22:18:40 -0400

    Show me the money

--- ball.py
+++ ball.py
@@ -82,3 +82,63 @@ class Ball(league.DUGameObject):
                         self.accel.y = 100
                         self.vel += self.jump
                         self.j = 1
+
+class Enemy(league.DUGameObject):
+    def __init__(self, engine, scene, start_x, start_y):
+        super().__init__()
+        self.engine = engine
+        self.scene = scene
+        self._layer =  1
+        self.dirty = 2
+        self.can_jump = False
+
+
+        # Making of our ball
+        self.image = pg.Surface((32, 32))
+        self.image.fill((150, 210, 220, 0))
+        self.image.set_alpha(255)
+        pg.draw.circle(self.image, (0, 101, 164), (16, 16), 16)
+
+        # Sets the Starting point of our Ball
+        self.rect = self.image.get_rect()
+        self.x = start_x
+        self.y = start_y
+        self.start_x = self.x
+        self.start_y = self.y
+        self.direction_x = 1
+        self.direction_y = 1
+
+    def update(self):
+
+    # Downwards acceleration
+        self.direction_x = 500
+        self.direction_y = 0
+
+        # Did we collide?
+        on_platform = False
+
+        in_bounds = True
+
+        for collideable in self.scene.collideables:
+            if self.rect.colliderect(collideable.rect):
+                if collideable.type == "torch":
+                    # self.scene.score += 1
+                    pass
+                elif collideable.type == "platform":
+                    on_platform = True
+
+        if on_platform:
+            self.direction_y = 0
+
+        while in_bounds:
+            # Moves our ball
+            if self.x > 800:
+                self.x = 0
+                self.y = self.start_y
+            self.x = self.x + self.engine.delta_time * self.direction_x
+            self.y = self.y + self.engine.delta_time * self.direction_y
+            self.rect.x = self.x
+            self.rect.y = self.y
+            in_bounds = False
+
+        # Moves our ball
--- main.py
+++ main.py
@@ -46,8 +46,27 @@ for x, y, image in map.layers[2].tiles():
     level_one.updateables.append(tmp)
     level_one.collideables.append(tmp)
 
+enemy = ball.Enemy(engine, level_one, 0, 400)
+enemy2 = ball.Enemy(engine, level_one, 0, 200)
+enemy3 = ball.Enemy(engine, level_one, 0, 560)
+enemy4 = ball.Enemy(engine, level_one, 0, 860)
 ball = ball.Ball(engine, level_one)
+level_one.drawables.append(enemy)
+level_one.updateables.append(enemy)
+enemy.type = "enemy"
+level_one.collideables.append(enemy)
+level_one.drawables.append(enemy2)
+level_one.updateables.append(enemy2)
+level_one.collideables.append(enemy2)
+enemy2.type = "enemy"
+level_one.drawables.append(enemy3)
+level_one.updateables.append(enemy3)
+level_one.collideables.append(enemy3)
+enemy3.type = "enemy"
+level_one.drawables.append(enemy4)
+level_one.updateables.append(enemy4)
+level_one.collideables.append(enemy4)
+enemy4.type = "enemy"
 level_one.drawables.append(ball)
 level_one.updateables.append(ball)
-
 engine.run()
